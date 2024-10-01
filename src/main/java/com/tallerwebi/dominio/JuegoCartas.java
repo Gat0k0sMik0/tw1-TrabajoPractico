898d22e0a287714cc5b4ef8f685b914153e32784
NotaMateria <fran.caco.fv@gmail.com>
2024-10-01 16:34:19 -0300

    ejemplo cartas

--- src/main/java/com/tallerwebi/dominio/JuegoCartas.java
+++ src/main/java/com/tallerwebi/dominio/JuegoCartas.java
@@ -1,13 +1,19 @@
 package com.tallerwebi.dominio;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
 public class JuegoCartas {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer numero;
