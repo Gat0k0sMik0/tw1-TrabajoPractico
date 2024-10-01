8b17c2c65880fa3d3ee911c61a008fa90def55d3
NotaMateria <fran.caco.fv@gmail.com>
2024-10-01 15:40:42 -0300

    conect database

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
 INSERT INTO Usuario(id, email, password, rol, activo)
-VALUES (null, 'test@unlam.edu.ar', 'test', 'ADMIN', true);
+VALUES (null, 'test@unlam.edu.ar', 'test', 'ADMIN', true),
+       (null, 'test2@unlam.edu.ar', 'test2', 'NULL', true);
