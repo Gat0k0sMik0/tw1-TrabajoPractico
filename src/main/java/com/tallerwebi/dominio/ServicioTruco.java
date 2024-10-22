78e77cab6dd363895cc49245940c710a65c37cfb
MartinZaccardo <zaccardomartin@gmail.com>
2024-10-22 20:02:05 -0300

    Cambios del envido

--- src/main/java/com/tallerwebi/dominio/ServicioTruco.java
+++ src/main/java/com/tallerwebi/dominio/ServicioTruco.java
@@ -11,6 +11,8 @@ public interface ServicioTruco {
     Boolean esTurnoJugador(String jugadorNombre);
     Jugador getTurnoJugador();
     void determinarGanadorRonda(Jugador jugador1, Jugador jugador2);
-
     void validarCartas(List<Carta> cartasJugador2);
+    Boolean esLaPrimerRonda(Jugador jugador1, Jugador jugador2);
+
+    void verificarEnvido(Jugador jugador1, Jugador jugador2);
 }
